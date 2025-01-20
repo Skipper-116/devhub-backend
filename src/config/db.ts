94be8142639a94f674980a66a0b10420f8ddb74f
Roy Chanunkha <roychanunkha@gmail.com>
2025-01-19 20:37:48 -0800

    chore: .env example

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
     try {
-        const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_URI}`;
+        const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`;
         await mongoose.connect(url);
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
